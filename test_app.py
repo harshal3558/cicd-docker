3b6a64bb48bbeef2cc2b42abb4a1278c13176c5f
Harshal <harshal3558@gmail.com>
2025-12-30 16:00:32 +0530

    docker ci cd yaml file ready

--- test_app.py
+++ test_app.py
@@ -1,1 +1,9 @@
 # Creating unit testing things
+
+from app import app
+
+def test_home():
+    response=app.test_client().get("/")
+
+    assert response.status_code==200
+    assert response.data== b"Hello World!"
